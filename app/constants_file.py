d70085e5ea7730c366d6ec84c26c1332f2df5b94
Jonathanrodrigr12 <53848036+Jonathanrodrigr12@users.noreply.github.com>
2022-11-01 16:40:55 -0500

    Update constants_file.py

--- app/constants_file.py
+++ app/constants_file.py
@@ -2,7 +2,7 @@ class Constants():
     secret = ""
     algoritm = "HS256"
     key_encryption = "ZmDfcTF7_60GrrY167zsiPd67pEvs0aGOv2oasOM1Pg="
-    SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:admin123@127.0.0.1:5432/postgres"
+    SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:admin123@db:5432/postgres"
     REDIS_URL= ""
     REDIS_PORT= ""
     REDIS_DB=0
