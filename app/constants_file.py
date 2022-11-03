1fb70941cfc2c93cf8ad457a8e58a8cb0fc17c44
Daury Lecca <jonathanrodrigr@gmail.com>
2022-11-02 19:35:59 -0500

    adjust code with comments and constant

--- app/constants_file.py
+++ app/constants_file.py
@@ -1,5 +1,5 @@
 class Constants():
-    secret = ""
+    secret = "secret"
     algoritm = "HS256"
     key_encryption = "ZmDfcTF7_60GrrY167zsiPd67pEvs0aGOv2oasOM1Pg="
     SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:admin123@db:5432/postgres"
